c3f6fd94a108a0842bd1ebcb0477b1bd4d594351
Trapperus <atle.vingalv@gmail.com>
2024-07-12 18:48:20 +0200

    Crosshair moves smoothly with camera, will edit the sensitivity a bit to make it match the gun movement better. Currently the crosshair moves faster than the gun does.

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Transform crosshairTarget;
+    public float sensitivity;
+
+    void Update()
+    {
+        float cameraMoveHorizontal = Input.GetAxis("Mouse X");
+        float cameraMoveVertical = Input.GetAxis("Mouse Y");
+
+        // Calculate the movement amount based on camera movement
+        Vector3 moveAmount = new Vector3(-cameraMoveHorizontal, -cameraMoveVertical, 0) * sensitivity;
+
+        // Apply the movement to the crosshair
+        transform.Translate(moveAmount, Space.Self);
+
+        // Smoothly move the crosshair back to the crosshair target
+        transform.position = Vector3.Lerp(transform.position, crosshairTarget.position, Time.deltaTime * 5.0f);
+    }
+}
